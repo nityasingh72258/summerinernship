cc627d2cf0462cfd0b613ef020a8553b791337cc
nityasingh72258 <nityasingh72258@gmail.com>
2026-06-18 11:18:09 +0530

    operators

---
+++ FD9/FD10/datatype.js
@@ -0,0 +1,38 @@
+//premitive data type->string,number,boolean,null,undeefine,bigint,symbol
+//non premitive ->array,object
+
+//string
+let name="nitya"
+console.log(name);
+console.log(typeof(name));
+
+//boolean
+let pass=true
+console.log(pass);
+console.log(typeof(pass));
+
+//number
+let a=12
+console.log(a);
+
+//undefine
+let x
+console.log(x);
+console.log(typeof(x));
+
+//null
+let x1=null
+console.log(x1);
+console.log(typeof(x1));
+
+//bigint
+let b=125238725435
+console.log(b);
+console.log(typeof(b));
+
+//symbole
+let id=symbole('nitu')
+let id2=symbole('nitu')
+console.log(id);
+console.log(id2);
+console.log(id==id2);
---
+++ FD9/FD10/operators.js
@@ -0,0 +1,42 @@
+//arithmetics OP->+,-,--,++,%,/,**
+
+a=2
+b=9
+console.log(a+b);
+
+//- subtraction
+a=6
+b=7
+console.log(b-a);
+
+//* multiplication
+a=5
+b=8
+h=9
+console.log(a*b*h);
+
+// / division
+a=18
+b=2
+console.log(a/b);
+
+// ** exponential
+a=3
+b=8
+console.log(a**b);
+
+//% module
+e=15
+m=4
+console.log(e%4);
+
+//++ increment
+num1=13
+console.log(num1++);
+console.log(num1);
+
+//-- increment
+num1=13
+console.log(num1--);
+console.log(num1);
+
