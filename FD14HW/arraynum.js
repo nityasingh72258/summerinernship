795414244950c261f461b53bb5b643bb76846d0e
nityasingh72258 <nityasingh72258@gmail.com>
2026-06-23 15:28:14 +0530

    array HOMEWORK

---
+++ FD14HW/arraynum.js
@@ -0,0 +1,5 @@
+//==array with loop===
+let numbers=[10,20,30,40,50]
+for(let i=0;i<=4;i++){
+    console.log(numbers[i]);
+}
---
+++ FD14HW/arraynumhw2.js
@@ -0,0 +1,7 @@
+//==creat an array and print their sum=====
+let nums=[30,40,10,20,]
+let sum=0;
+for(let num of nums){
+    sum+=num;
+}
+console.log(sum);
