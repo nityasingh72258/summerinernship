88ed75a4feb5a373e0c018adb26c964339d8d5c6
nityasingh72258 <nityasingh72258@gmail.com>
2026-06-23 11:15:50 +0530

    array in js

---
+++ FD14/ARRAYmethod.js
@@ -0,0 +1,25 @@
+//====method are use====
+//====first push() to insert element in the last=====
+let name=["riya","nitya","nitin","rita"]
+name.push("anuj")
+console.log(name);
+//unshift() to insert element at the first===
+name.unshift("aryan")
+console.log(name);
+
+//pop() delete to last
+name.pop()
+console.log(name);
+
+//shift() delete tat the first
+name.shift(0)
+console.log(name);
+
+//indexof====to find index number======
+find=name.indexOf("nitya")
+console.log(find);
+
+//includes(to check element are present are not)=====
+num=name.includes("nitin")
+console.log(num);
+
---
+++ FD14/arrayp1.js
@@ -0,0 +1,9 @@
+let num=[10,20,30,40,50,60]
+res=num[2]+num[3]
+console.log("add:",res);
+for(let i=0;i<=5;i++){
+    if(num[i]=="40"){
+        break
+    }
+    console.log(num[i]);
+}
