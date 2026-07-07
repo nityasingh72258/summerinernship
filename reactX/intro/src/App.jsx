cd857297a8c44153d3f526a00a1294101590eb95
nityasingh72258 <nityasingh72258@gmail.com>
2026-07-07 19:22:48 +0530

    react

---
+++ reactX/intro/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Greet from './Greet'
+const App = () => {
+  return (
+    <div>
+      <Greet/>
+      <Greet/>
+      <Greet/>
+    </div>
+  )
+}
+
+export default App
---
+++ reactX/intro/src/Greet.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <div>
+        <h1 className='heading'>hello world</h1>
+        <p id='pr'>modi ji</p>
+    </div>
+  )
+}
+
+export default Greet
